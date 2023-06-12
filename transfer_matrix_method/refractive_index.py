e7ebf6d5d471f63fef9349c4877ff9ec600049bb
Charlie Kennedy <charleslloydkennedy@gmail.com>
2023-06-12 14:40:48 +0100

    add method for interpolating between data points

--- transfer_matrix_method/refractive_index.py
+++ transfer_matrix_method/refractive_index.py
@@ -10,5 +10,15 @@ class RefractiveIndex:
         self._data = {}
         for data_point in data:
             if not len(data_point) == 0:
-                self._data[data_point[0]] = {"n": data_point[1], "k": data_point[2]}
+                self._data[float(data_point[0])] = {"n": float(data_point[1]), "k": float(data_point[2])}
+
+    def __getitem__(self, wavelength:float):
+        wavelengths = self._data.keys()
+        below = max([w for w in wavelengths if w <= wavelength])
+        above = min([w for w in wavelengths if w > wavelength])
+        interp_coeff = (wavelength-below) / (above - below)
+        return {
+            "n": self._data[below]["n"] + interp_coeff * (self._data[above]["n"] - self._data[below]["n"]), 
+            "k": self._data[below]["k"] + interp_coeff * (self._data[above]["k"] - self._data[below]["k"])
+        }
 
