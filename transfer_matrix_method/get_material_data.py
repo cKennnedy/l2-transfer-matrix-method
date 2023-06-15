efc4751480af37537ccfafc1712e27da9510598a
Charlie Kennedy <charleslloydkennedy@gmail.com>
2023-06-15 15:39:23 +0100

    add test train split

--- transfer_matrix_method/get_material_data.py
+++ transfer_matrix_method/get_material_data.py
@@ -5,6 +5,6 @@ directory = pathlib.Path(__file__).parent/"data"
 def get_filepath(material: str) -> str:
     path = directory/f"{material}.txt"
     if not path.is_file():
-        raise ValueError("Material not found in data library")
+        raise ValueError(f"Material \"{material}\" not found in data library")
     
     return path
